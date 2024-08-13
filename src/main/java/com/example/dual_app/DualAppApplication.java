b4ac26ba10dd8da2aa2017f423ead8e44f75b421
Dias Rakhman <rahmanovdias13@gmail.com>
2024-08-13 21:51:12 +0500

    Initial commit

---
+++ src/main/java/com/example/dual_app/DualAppApplication.java
@@ -0,0 +1,18 @@
+package com.example.dual_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DualAppApplication {
+
+	public static void main(String[] args) {
+
+
+
+		SpringApplication.run(DualAppApplication.class, args);
+
+
+	}
+
+}
