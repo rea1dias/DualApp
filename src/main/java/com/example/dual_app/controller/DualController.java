4dccf5aca5fc5b5b3b9c02f16ae88a01e1089389
Dias Rakhman <rahmanovdias13@gmail.com>
2024-08-14 18:19:12 +0500

    Исправлена сборка проекта maven и основная страница сайта

--- src/main/java/com/example/dual_app/controller/DualController.java
+++ src/main/java/com/example/dual_app/controller/DualController.java
@@ -1,8 +1,7 @@
 package com.example.dual_app.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class DualController {
